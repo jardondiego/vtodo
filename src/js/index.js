fc1594a56c586bd290baf5b17773fda796bd5caa
Diego Jardon <diegojardonc@gmail.com>
2018-06-30 21:23:03 -0500

    Masonry layout implemented

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,18 @@
 import List from './list'
-import '../css/state.css'
+import '../css/index.css'
 
 let addTodoListForm = document.getElementById('add-todo-list')
+let masonryColumns = Array.from(document.getElementById('content').children)
+let whereDoesItGo = 1
+
+console.log(masonryColumns)
 
 addTodoListForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    document.body.append(List(e.target.firstElementChild.value))
+    e.preventDefault()
+    if (e.target.firstElementChild.value.trim() === '') return
+    if (whereDoesItGo === 4) 
+        whereDoesItGo = 1
+    masonryColumns[whereDoesItGo-1].append(List(e.target.firstElementChild.value))
+    whereDoesItGo++
     e.target.firstElementChild.value = ''
 })
