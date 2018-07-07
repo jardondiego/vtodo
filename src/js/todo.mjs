2fd4ab1141d20a999d77dcd8804af17d7a9f311f
Diego Jardon <diegojardonc@gmail.com>
2018-07-06 20:46:01 -0500

    changed to css class toggling approach instead on inline

--- src/js/todo.mjs
+++ src/js/todo.mjs
@@ -17,13 +17,13 @@ export default function Todo (name) {
         check.classList.add('todo__do')
         title.classList.add('todo__title')
         editTodo.classList.add('todo__edit-todo-title')
-        editTodo.classList.add('is-hidden')
+        editTodo.classList.add('todo__edit-todo-title--is-hidden')
             editTodoInput.classList.add('todo__edit-todo-title__title')
             editTodoInput.value = name
             editTodoSubmit.classList.add('todo__edit-todo-title__edit')
         remove.classList.add('todo__remove')
         editTodoSubmit.type = 'submit'
-        editTodoSubmit.innerText = 'Edit'
+        editTodoSubmit.innerText = '✏'
         check.type = 'checkbox'
         title.innerText = name
         editTodo.type = 'text'
@@ -43,16 +43,16 @@ export default function Todo (name) {
 
     // On click for todo show form
     title.addEventListener('click', function (e) {
-        e.target.style.display = 'none'
-        e.target.nextElementSibling.style.display = 'initial'
+        title.classList.toggle('todo__title--is-hidden')
+        editTodo.classList.toggle('todo__edit-todo-title--is-hidden')
         e.target.nextElementSibling.firstElementChild.value = e.target.innerText
         e.target.nextElementSibling.firstElementChild.focus()
     })
 
     // On input blur hide form
     editTodoInput.addEventListener('blur', function (e) {
-        e.target.parentElement.style.display = 'none'
-        e.target.parentElement.previousElementSibling.style.display = 'initial'
+        editTodo.classList.toggle('todo__edit-todo-title--is-hidden')
+        title.classList.toggle('todo__title--is-hidden')
 
         if (e.target.value === undefined || e.target.value === '') return
 
